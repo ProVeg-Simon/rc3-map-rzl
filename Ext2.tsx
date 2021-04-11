cdfa7913e44332002889dd07fb38feed0b37cac0
deBaer <37374038+ProVeg-Simon@users.noreply.github.com>
2021-04-11 03:24:27 +0200

    Revert "udtrane"
    This reverts commit add504cfea0ffc73b95832fc7d802f7ac9a27567.

--- Ext2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.5.0" name="Ext2" tilewidth="32" tileheight="32" tilecount="91" columns="7">
- <image source="rwd-cache-3d-printing-assembly.maps.at.rc3.world-tiles-png-deco-minideko.png-1ed08531.png" trans="000000" width="224" height="416"/>
-</tileset>
--- Ext3.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.5.0" name="Ext3" tilewidth="32" tileheight="32" tilecount="1024" columns="16">
- <image source="rwd-cache-b3.maps.at.rc3.world-tiles-png-community-cert-imgs-tilesheets-misc.png-edf1e997.png" trans="000000" width="512" height="2048"/>
-</tileset>
